9becc21464377b70fd9d76ebeb2a127aacecba3e
Shifath Salam <shifath.salam.216@my.csun.edu>
2019-11-06 00:23:31 -0800

    card component with props

--- client/src/components/pkmn/Card.js
+++ client/src/components/pkmn/Card.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
 export default class Card extends Component {
+  state = {
+    name: "",
+    sprite: "",
+    number: ""
+  };
+  componentDidMount() {
+    const { name, url } = this.props;
+    const number = url.split("/")[url.split("/").length - 2];
+    const sprite = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${number}.png?raw=true`;
+    this.setState({ name, sprite, number });
+  }
   render() {
     return (
-      <div className="col-md-4 col-sm-6 mb-5">
+      <div className="col-md-4 col-sm-6">
         <div className="card">
-          <div className="card-header">Pokemon</div>
+          <div className="card-header">{this.state.number}</div>
+          <div className="card-body">
+            <h6>
+              <div className="card-title">{this.state.name}</div>
+            </h6>
+          </div>
         </div>
       </div>
     );
--- client/src/components/pkmn/List.js
+++ client/src/components/pkmn/List.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
 import Card from "./Card";
+import axios from "axios";
 export default class List extends Component {
+  state = {
+    url: "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0%22",
+    pokemon: null
+  };
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data["results"] });
+  }
   render() {
     return (
       <div>
-        <Card />
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map(pokemon => (
+              <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading</h1>
+        )}
       </div>
     );
   }
